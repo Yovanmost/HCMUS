74c7c734fb5d41865038d74df8134b2921e282a8
Yovanmost <yovanmost@gmail.com>
2024-02-03 18:24:53 +0700

    Assignment 3.1 - Ex 1

--- C++/OS/Assingment_03.01/Ex1.cpp
+++ C++/OS/Assingment_03.01/Ex1.cpp
@@ -4,24 +4,28 @@
 #include <string>
 #include <locale>
 #include <codecvt>
+#include "binaryFileWriting.cpp"
 
 using namespace std;
 
-std::wstring s2ws(const std::string& str)
+// function for converting wstring to string and vice versa
+wstring s2ws(const string& str)
 {
-    using convert_typeX = std::codecvt_utf8<wchar_t>;
-    std::wstring_convert<convert_typeX, wchar_t> converterX;
+    using convert_typeX = codecvt_utf8<wchar_t>;
+    wstring_convert<convert_typeX, wchar_t> converterX;
 
     return converterX.from_bytes(str);
 }
 
-std::string ws2s(const std::wstring& wstr)
+string ws2s(const wstring& wstr)
 {
-    using convert_typeX = std::codecvt_utf8<wchar_t>;
-    std::wstring_convert<convert_typeX, wchar_t> converterX;
+    using convert_typeX = codecvt_utf8<wchar_t>;
+    wstring_convert<convert_typeX, wchar_t> converterX;
 
     return converterX.to_bytes(wstr);
 }
+
+// create folder function
 
 bool createFolder(const wchar_t* path){
     if (CreateDirectoryW(path, NULL) || GetLastError() == ERROR_ALREADY_EXISTS){
@@ -37,19 +41,35 @@ bool createHiddenFolder(const wchar_t* path){
         DWORD attributes = GetFileAttributesW(path);
         if (attributes != INVALID_FILE_ATTRIBUTES) {
             SetFileAttributesW(path, attributes | FILE_ATTRIBUTE_HIDDEN);
+            cout << "Hidden folder created successfully!" << '\n';
             return true;
         }
     }
     return false;
 }
 
+// create file function
+
 bool createTextFile(string filePath, string content) {
     ofstream out(filePath);
     if (out.is_open()){
         out << content;
+        cout << ".txt file created successfully!\n";
         out.close();
         return true;
     }
+    cout << "failed to create .txt!\n";
+    return false;
+}
+
+bool createOSFile(string filePath){
+    ofstream out(filePath);
+    if (out.is_open()){
+        out.close();
+        cout << ".os file created successfully!\n";
+        return true;
+    }
+    cout << "failed to create .os file!\n";
     return false;
 }
 
@@ -67,20 +87,27 @@ int main() {
     // main folder
     createFolder(mainFolderName);
 
+    // os file in main folder
+    wstring filePath1 = wstring(mainFolderName) + L"\\" + L"Data1.os";
+    createOSFile(ws2s(filePath1));
+
     // path for the 1st subfolder
-    std::wstring subFolderPath1 = std::wstring(mainFolderName) + L"\\" + subFolderName1;
+    wstring subFolderPath1 = wstring(mainFolderName) + L"\\" + subFolderName1;
     // subfolder 1 creation
     createFolder(subFolderPath1.c_str());
 
     // path for the 2nd subfolder
-    std::wstring subFolderPath2 = std::wstring(mainFolderName) + L"\\" + subFolderName2;
+    wstring subFolderPath2 = wstring(mainFolderName) + L"\\" + subFolderName2;
     // subfolder 2 creation
     createHiddenFolder(subFolderPath2.c_str());
 
     // text file in 2nd subfolder
-    wstring filePath = subFolderPath2 + L"\\" + L"Data2.txt";
-    createTextFile(ws2s(filePath), content);
+    wstring filePath2 = subFolderPath2 + L"\\" + L"Data2.txt";
+    createTextFile(ws2s(filePath2), content);
 
+    // binary file in 1st subfolder
+    wstring filePath3 = subFolderPath1 + L"\\" + L"Data3.bin";
+    createBinaryFile(ws2s(filePath3));
 
     return 0;
 }
